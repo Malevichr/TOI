0699c815eac6b3d05451e2c70a0a9c1e422663c7
Danila <rexerofficial@gmail.com>
2024-02-26 15:14:03 +0300

    Refractor of directories

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import calendar
-import random
-
-from Include.labs.Lab1 import Lab1
-from Include.labs.Lab2 import Lab2
+from labs.Lab1 import Lab1
 
 if __name__ == '__main__':
     Lab1(int(input())).start()
